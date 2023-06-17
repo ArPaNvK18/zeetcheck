5f6413abaa92716a11aa12bf0930293ea62c3280
ARPANVK <arpansamanta08@gmail.com>
2023-06-17 11:09:21 +0530

    first commit

---
+++ GolpoSolpo/blog/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path,include
+from . import views
+
+admin.site.site_header = "GolpoSolpo Admin"
+admin.site.site_title = "GolpoSolpo"
+# admin.site.index_header = "Index"
+
+urlpatterns = [
+    path('contact',views.Contact),
+    path('post/<slug:url>/',views.firstpage),
+    path('catagory/<slug:url>/',views.ViewCatagory),
+]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
